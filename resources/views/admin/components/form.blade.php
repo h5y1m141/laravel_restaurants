a21216b5e115f2266d93aff5501bdbb9b64b1269
HiroshiOyamada <h5y1m141@gmail.com>
2018-04-14 07:33:21 +0900

    multipartで画像アップロード出来るようにフォーム修正

--- resources/views/admin/components/form.blade.php
+++ resources/views/admin/components/form.blade.php
@@ -6,4 +6,13 @@
   <label for="address">所在地</label>
   <input id="address" type="text" class="form-control" name="address" value="{{$restaurant->address}}" required>
 </div>
+<div class="form-group">
+  <div>
+    @if ($restaurant->picture_filename)
+      <img width="100" height="50" alt="店舗画像" src="{{ asset('storage/' . $restaurant->picture_filename) }}"/>
+    @endif
+  </div>
+  <label for="address">店舗画像</label>
+  <input name="picture_filename" type="file" />
+</div>
 <button type="submit" name="submit" class="btn btn-primary">{{ $submit }}</button>
