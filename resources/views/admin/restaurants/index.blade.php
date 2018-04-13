f8b692c2f987c0f45e6f27d731a1c68cd68fdffd
HiroshiOyamada <h5y1m141@gmail.com>
2018-04-13 18:33:15 +0900

    編集画面へのリンク設定

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -24,11 +24,26 @@
         店舗一覧
       </h2>
       <table class="table">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>店舗名</th>
+            <th>住所</th>
+            <th>作成日</th>
+            <th>編集</th>
+          </tr>
+        </thead>
         @foreach ($restaurants as $restaurant)
           <tr>
             <td>{{ $restaurant->id }}</td>
             <td>{{ $restaurant->name }}</td>
+            <td>{{ $restaurant->address }}</td>
             <td>{{ $restaurant->created_at }}</td>
+            <td>
+              <a href="/admin/restaurants/{{ $restaurant->id }}/edit">
+                <button class="btn btn-info">編集する</button>
+              </a>
+            </td>
           </tr>
         @endforeach
       </table>
